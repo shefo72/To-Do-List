166258797a918885de166900242c8356c4381774
Ahmed Sherif <ahmedsherifeg22@gmail.com>
2025-12-30 15:49:47 +0200

    validate data with Notyf

--- js/main.js
+++ js/main.js
@@ -5,8 +5,12 @@ const noteBody = document.getElementById("noteBody");
 
 const tasks = [];
 
+const notyf = new Notyf();
 addBtn.addEventListener("click", () => {
-  if (!noteTitle.value || !noteBody.value) return;
+  if (!noteTitle.value || !noteBody.value) {
+    notyf.error("You must fill out all data moving forward");
+    return;
+  }
 
   const task = {
     id: Date.now(),
@@ -20,6 +24,7 @@ addBtn.addEventListener("click", () => {
   noteBody.value = "";
 
   viewTasks(tasks);
+  notyf.success("Your note have been successfully added!");
 
   const modalEl = document.getElementById("exampleModal");
   const modal = bootstrap.Modal.getInstance(modalEl);
@@ -31,30 +36,32 @@ function viewTasks(tasks) {
 
   tasks.forEach((task) => {
     TasksHTML += `
-      <div class="col-lg-4 col-sm-6 d-flex">
-        <div class="task bg-red p-3 rounded-2 w-100">
-          <h3>${task.title}</h3>
-          <p>${task.body}</p>
-          <button class="btn btn-sm btn-danger mt-2" onclick="deleteTask(${task.id})">Delete</button>
-        </div>
-      </div>
-    `;
+          <div class="col-lg-4 col-sm-6 d-flex">
+            <div class="task bg-red p-3 rounded-2 w-100">
+              <h3>${task.title}</h3>
+              <p>${task.body}</p>
+              <button class="noteDeleteBtn btn btn-sm mt-2" onclick="deleteTask(${task.id})">
+                <i class="fa-solid fa-x"></i>
+              </button>
+            </div>
+          </div>
+      `;
   });
 
   TasksHTML += `
-    <div class="col-lg-4 col-sm-6 d-flex">
-      <div class="task bg-secondary-subtle p-3 rounded-2 w-100 d-flex align-items-center justify-content-center">
-        <button
-          class="w-100 h-100 btn"
-          type="button"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          <i class="fa-solid fs-1 fa-plus"></i>
-        </button>
+      <div class="col-lg-4 col-sm-6 d-flex">
+        <div class="task bg-secondary-subtle p-3 rounded-2 w-100 d-flex align-items-center justify-content-center">
+          <button
+            class="w-100 h-100 btn"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            <i class="fa-solid fs-1 fa-plus"></i>
+          </button>
+        </div>
       </div>
-    </div>
-  `;
+    `;
 
   container.innerHTML = TasksHTML;
 }
@@ -65,6 +72,7 @@ function deleteTask(id) {
     tasks.splice(index, 1);
     viewTasks(tasks);
   }
+  notyf.success("Your note have been successfully deleted!");
 }
 
 viewTasks(tasks);
